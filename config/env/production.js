92302be2bd66347044a78491e01ff9e8217349ab
Rehnan Carolino <rehnancarolino@live.com>
2015-03-04 12:52:22 -0300

    Deploy to Heroku

--- config/env/production.js
+++ config/env/production.js
@@ -26,10 +26,4 @@ module.exports = {
    * Set the default database connection for models in the production        *
    * environment (see config/connections.js and config/models.js )           *
    ***************************************************************************/
-
-   models: {
-     connection: 'MongodbProd'
-   },
-
-
 };
