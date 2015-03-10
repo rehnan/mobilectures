24dc2cce1460b589f7f02cdb3a0e5e596d24d846
Rehnan Carolino <rehnancarolino@live.com>
2015-03-10 02:06:49 -0300

    password connection db produtcion changed

--- config/models.js
+++ config/models.js
@@ -28,7 +28,7 @@ module.exports.models = {
   * See http://sailsjs.org/#/documentation/concepts/ORM/model-settings.html  *
   *                                                                          *
   ***************************************************************************/
-   migrate: 'drop',
+   migrate: 'alter',
    //connections: 'development'
-   connection: 'production',
+   connection: 'localDiskDb',
 };
