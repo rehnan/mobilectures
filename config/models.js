52e9489de93a8d249b5754403edd2d1a84c4b522
Rehnan Carolino <rehnancarolino@live.com>
2015-05-18 11:52:01 -0300

    Setting to production connection

--- config/models.js
+++ config/models.js
@@ -29,7 +29,7 @@ module.exports.models = {
   *                                                                          *
   ***************************************************************************/
    migrate: 'alter',
-   connection: 'development'
-   //connection: 'production'
+   //connection: 'development'
+   connection: 'production'
    //connection: 'localDiskDb',
 };
