1006d10bc44e6b95e18173a569e9ecd29a5dc1f3
Rehnan Carolino <rehnancarolino@live.com>
2015-06-02 14:52:18 -0300

    File polls.js included

--- assets/js/polls.js
+++ assets/js/polls.js
@@ -1,8 +1,8 @@
-$(document).ready(function (){  ml.polls.load() });
+$(document).ready(function (){ alert('hiiii!');  ml.polls.load() });
 
 ml.polls = {
 
 	load: function () {
 		alert('hiiii!');
-	},
-}
+	}
+};
